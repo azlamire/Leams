16a7d01fd082509ef91d25e1aabef8aeeff30c3d
juchiLee@proton.me <juchiLee@proton.me>
2025-07-27 18:56:12 +0300

    File changed
    Recovered branch

---
+++ src/shared/data/stream.data.ts
@@ -0,0 +1,55 @@
+import type { Stream } from "../types/stream.interface";
+
+export const STREAMS: Stream[] = [
+  {
+    author: "Hmmmm?",
+    viewers: 50,
+    longliness: "1m"
+  },
+  {
+    author: "TechWhiz42",
+    viewers: 320,
+    longliness: "5m"
+  },
+  {
+    author: "DeepThoughtBot",
+    viewers: 1500,
+    longliness: "12m"
+  },
+  {
+    author: "LofiCoder",
+    viewers: 215,
+    longliness: "35m"
+  },
+  {
+    author: "AstralNomad",
+    viewers: 800,
+    longliness: "1h"
+  },
+  {
+    author: "SassySyntax",
+    viewers: 120,
+    longliness: "2h"
+  },
+  {
+    author: "QuantumQuokka",
+    viewers: 675,
+    longliness: "4h"
+  },
+  {
+    author: "NeuralNetNerd",
+    viewers: 9200,
+    longliness: "8h"
+  },
+  {
+    author: "CoffeeOverflow",
+    viewers: 60,
+    longliness: "9h"
+  },
+  {
+    author: "404NotFamous",
+    viewers: 15,
+    longliness: "22h"
+  }
+];
+

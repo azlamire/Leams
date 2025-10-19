eb40e7b0a3af204fd85675c67f2876ba650d3454
azlamire <juchiLee@proton.me>
2025-10-19 08:04:35 +0300

    Files changed
    Made underlines in reg module page

--- src/app/_components/LogReg/MainLogReg.tsx
+++ src/app/_components/LogReg/MainLogReg.tsx
@@ -8,114 +8,114 @@ import cslx from "clsx";
 import { Login } from "./Login";
 import { AnimatePresence, LayoutGroup, motion } from "motion/react";
 
-
-
 type RegLogButtsType = {
-    name: string;
-    active: boolean;
-    onClick: () => void;
-    className: string;
+	name: string;
+	active: boolean;
+	onClick: () => void;
+	className: string;
 }
 
 const underline: React.CSSProperties = {
-    position: "absolute",
-    bottom: -2,
-    left: 0,
-    right: 0,
-    height: 2,
-    background: "var(--accent)",
+	position: "absolute",
+	bottom: -2,
+	left: 0,
+	right: 0,
+	height: 2,
+	background: "var(--accent)",
 }
 
-
-
 export function MainLogin() {
-    const isLogin = useStore(store, (state) => state.isLogin);
-    const [testing, setTesting] = useState(false)
-    const opened: boolean = useStore(store, (state) => state.openReg);
-    const butRegLog: RegLogButtsType[] = [
-        {name: "Log In", active: isLogin, onClick: () => store.setState(prev => ({...prev, isReg: false, isLogin: true})), className : cslx("cursor-pointer", {
-            "border-b-0" : isLogin,
-    
-        })},
-        {name: "Sign Up",active: !isLogin, onClick: () => store.setState(prev => ({...prev, isReg: true, isLogin: false})), className : cslx("cursor-pointer ", {
-            "border-b-0" : !isLogin,
+	const isLogin = useStore(store, (state) => state.isLogin);
+	const [testing, setTesting] = useState(false)
+	const opened: boolean = useStore(store, (state) => state.openReg);
+	const butRegLog: RegLogButtsType[] = [
+		{
+			name: "Log In", active: isLogin, onClick: () => store.setState(prev => ({ ...prev, isReg: false, isLogin: true })), className: cslx("cursor-pointer", {
+				"border-b-0": isLogin,
 
-        })},
-    ]
-    useEffect(() => {
-        if (opened) {
-            document.body.style.overflow = "hidden";
-            const handleKeyDown = (event: KeyboardEvent) => {
-                if (event.key == "Escape") {
-                    store.setState(prev => ({...prev, openReg: false }));
-                }
-            }
-            document.addEventListener("keydown", handleKeyDown);
-            return () => {
-                document.removeEventListener("keydown", handleKeyDown);
-            };
-        }
-        else if (!opened){
-            document.body.style.overflow = "auto";
-        }
-    }, [opened]);
-    return(
-        <>
-            {createPortal(
-                <AnimatePresence mode="wait">
-                    {opened && (
-                        <motion.div 
-                        className="fixed inset-0 w-full h-full bg-[#00000090]"
-                        initial={{opacity: 0}}
-                        exit={{opacity: 0}}
-                        animate={{opacity: 1}}
-                        transition={{duration: 0.3}}>
-                            <div className="flex items-center justify-center h-full">
-                                <div className="relative w-[26%] bg-[#111b2fc0] shadow-xl rounded-lg flex text-bamble items-center flex-col p-5 gap-8">
-                                    <div className="relative h-10 w-full">
-                                        <button 
-                                        className="absolute bg-neutral-100 right-0 top-0 rounded-full h-[30px] w-[30px] shadow-xl flex flex-col justify-center items-center 
+			})
+		},
+		{
+			name: "Sign Up", active: !isLogin, onClick: () => store.setState(prev => ({ ...prev, isReg: true, isLogin: false })), className: cslx("cursor-pointer ", {
+				"border-b-0": !isLogin,
+
+			})
+		},
+	]
+	useEffect(() => {
+		if (opened) {
+			document.body.style.overflow = "hidden";
+			const handleKeyDown = (event: KeyboardEvent) => {
+				if (event.key == "Escape") {
+					store.setState(prev => ({ ...prev, openReg: false }));
+				}
+			}
+			document.addEventListener("keydown", handleKeyDown);
+			return () => {
+				document.removeEventListener("keydown", handleKeyDown);
+			};
+		}
+		else if (!opened) {
+			document.body.style.overflow = "auto";
+		}
+	}, [opened]);
+	return (
+		<>
+			{createPortal(
+				<AnimatePresence mode="wait">
+					{opened && (
+						<motion.div
+							className="fixed inset-0 w-full h-full bg-[#00000090]"
+							initial={{ opacity: 0 }}
+							exit={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							transition={{ duration: 0.3 }}>
+							<div className="flex items-center justify-center h-[100%]">
+								<div className="relative w-[26%] bg-[#efe6de] shadow-xl rounded-lg flex text-[#373539] items-center flex-col p-5 gap-8">
+									<div className="relative h-10 w-full">
+										<button
+											className="absolute bg-neutral-100 right-0 top-0 rounded-full h-[30px] w-[30px] shadow-xl flex flex-col justify-center items-center 
                                         transition-all duration-300 hover:bg-gray-200 focus:bg-gray-400"
-                                        onClick={() => store.setState(prev => ({...prev, openReg: false}))}> 
-                                            <span 
-                                            className="bg-[#181A1B] h-[4px] w-[100%] block transition-all 
+											onClick={() => store.setState(prev => ({ ...prev, openReg: false }))}>
+											<span
+												className="bg-[#181A1B] h-[4px] w-[100%] block transition-all 
                                             duration-300 rotate-50 translate-y-[3px]"></span>
-                                            <span 
-                                            className="bg-[#181A1B] h-[4px] w-[100%] block transition-all 
+											<span
+												className="bg-[#181A1B] h-[4px] w-[100%] block transition-all 
                                             duration-300 rotate-132 -translate-y-[3px]"></span>
-                                        </button>
-                                    </div>
-                                    <div className="h-10 w-full flex flex-row shadow-sm-b gap-10 font-bold text-[20px] border-b-3 border-gray-500 z-0 mb-5">
-                                        <LayoutGroup>
-                                            {butRegLog.map((item, index) => (
-                                                <div key={item.name} className="relative w-60% flex flex-row">
-                                                    <button
-                                                        className={item.className}
-                                                        onClick={item.onClick}
-                                                    >
-                                                        {item.name}
-                                                    </button>
-                                                    {item.active && (
-                                                        <motion.div
-                                                            className="z-20 absolute -bottom-[3px] w-full h-[3px] bg-sakura"
-                                                            layoutId="underline"
-                                                            id="underline"
-                                                            
-                                                        />
-                                                    )}
-                                                </div>
-                                            ))}
-                                        </LayoutGroup>
-                                    </div>
-                                    { isLogin ? <Login /> : <SignIn /> }
-                                    
-                                </div>
-                            </div>
-                        </motion.div>
-                    )}
-                    </AnimatePresence>,
-                document.body
-            )}
-        </>
-    )
+										</button>
+									</div>
+									<div className="h-10 w-full flex flex-row shadow-sm-b gap-10 font-bold text-[20px] border-b-3 border-gray-500 z-0 mb-5">
+										<LayoutGroup>
+											{butRegLog.map((item, index) => (
+												<div key={item.name} className="relative w-60% flex flex-row">
+													<button
+														className={item.className}
+														onClick={item.onClick}
+													>
+														{item.name}
+													</button>
+													{item.active && (
+														<motion.div
+															className="z-20 absolute -bottom-[3px] w-full h-[3px] bg-sakura"
+															layoutId="underline"
+															id="underline"
+
+														/>
+													)}
+												</div>
+											))}
+										</LayoutGroup>
+									</div>
+									{isLogin ? <Login /> : <SignIn />}
+
+								</div>
+							</div>
+						</motion.div>
+					)}
+				</AnimatePresence>,
+				document.body
+			)}
+		</>
+	)
 }
--- src/app/_components/LogReg/SignIn.tsx
+++ src/app/_components/LogReg/SignIn.tsx
@@ -1,191 +1,217 @@
-import { time } from "console";
-import type Email from "next-auth/providers/email";
 import { Roboto } from "next/font/google";
-import { useEffect, useRef, useState, type Dispatch, type FormEventHandler, type SetStateAction } from "react";
+import { useRef, useState, type Dispatch, type FormEventHandler, type SetStateAction } from "react";
 import { useRouter } from 'next/navigation'
 import clsx from "clsx";
-import { FaGithubAlt } from "react-icons/fa";
-import { url } from "inspector";
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa6";
 
 type FormType = {
-    name: string;
-    label: string;
-    type: string;
-    on_change?: (value: string, hook: Dispatch<SetStateAction<boolean>>) => void;
+	name: string;
+	label: string;
+	type: string;
+	on_change?: (value: string, hook: Dispatch<SetStateAction<boolean | undefined>>) => void;
 }
 
 type FormStateType = {
-    username: string;
-    password: string;
-    email: string
+	username: string;
+	password: string;
+	email: string
 }
 
 const signInForm: FormType[] = [
-    {name: "username", label: "Username",type:"text",
-        on_change: async (value: string, hook: Dispatch<SetStateAction<boolean>>) => {
-            const some_request = await fetch("http://127.0.0.1:8000/has_user", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({nickname: value})
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                console.log(data.invalidUser);
-                hook(data.invalidUser);
-            })
-        }
-    },
-    {name: "password", label: "Password", type: "password"},
-    {name: "email", label: "Email",type: "email", 
-        on_change: async (value: string) => {
-            const some_request = await fetch("http://127.0.0.1:8000/has_email", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({Email: value})
-            })
-        }
-    },  
+	{
+		name: "username", label: "Username", type: "text",
+		on_change: async (value: string, hook: Dispatch<SetStateAction<boolean | undefined>>) => {
+			const some_request = await fetch("http://127.0.0.1:8000/has_user", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify({ nickname: value })
+			})
+				.then(response => response.json())
+				.then(data => {
+					console.log(data);
+					console.log(data.invalidUser);
+					hook(data.invalidUser);
+				})
+		}
+	},
+	{ name: "password", label: "Password", type: "password" },
+	{
+		name: "email", label: "Email", type: "email",
+		on_change: async (value: string, hook: Dispatch<SetStateAction<boolean | undefined>>) => {
+			const some_request = await fetch("http://127.0.0.1:8000/has_email", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify({ Email: value })
+			})
+				.then(response => response.json())
+				.then(data => {
+					hook(data.invalidEmail);
+				})
+		}
+	},
 ];
 
 const roboto = Roboto({
-    subsets: ['latin'],
-  })
+	subsets: ['latin'],
+})
 
 export function SignIn() {
-    const router = useRouter()
-    const [nickValid, setNickValid] = useState<boolean>(false)
-    const [passValid, setPassValid] = useState<boolean>(false)
-    const [form, setForm] = useState<FormStateType>({
-        username: "",
-        password: "",
-        email: "",
-    })
+	const router = useRouter()
+	// NOTE: idk is it really worth to make mutliple or remain the big one
+	const [nickValid, setNickValid] = useState<boolean | undefined>(undefined)
+	const [passValid, setPassValid] = useState<boolean | undefined>(undefined)
+	const [passType, setPassType] = useState<boolean>(true)
+	const [emailValid, setEmailValid] = useState<boolean | undefined>(undefined)
+	const [form, setForm] = useState<FormStateType>({
+		username: "",
+		password: "",
+		email: "",
+	})
 
-    const timeoutRefs = useRef<{[key: string]: NodeJS.Timeout}>({})
+	const timeoutRefs = useRef<{ [key: string]: NodeJS.Timeout }>({})
 
-    const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
-        console.log("Form submitted", form);
-        e.preventDefault();
-        const response = await fetch("http://127.0.0.1:8000/register", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(form)
-        })
-        const Content = await response.json();
-        console.log(Content);
-    }
+	const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+		console.log("Form submitted", form);
+		e.preventDefault();
+		const response = await fetch("http://127.0.0.1:8000/register", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(form)
+		})
+		const Content = await response.json();
+		console.log(Content);
+	}
 
-    const GitHubAuth = async () => {
-        const response = await fetch("http://127.0.0.1:8000/auth/github", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            },
-        })
-        const Content = await response.json();
-        router.push(Content)
-    }
+	const GitHubAuth = async () => {
+		const response = await fetch("http://127.0.0.1:8000/auth/github", {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json"
+			},
+		})
+		const Content = await response.json();
+		router.push(Content)
+	}
 
-    return (
-        <>
-            <form className="flex flex-col rounded-xl w-full h-[65%] gap-7" onSubmit={handleSubmit}>
-                {
-                    signInForm.map((item, index) => (
-                        <div key={item.name} className={clsx(
-                            "relative flex gap-2 flex-col w-full mb-5 group border-b-0 [background:linear-gradient(#3b82f6_0_0)_bottom/var(--d,0)_3px_no-repeat] transition-all duration-500 [--d:0]", {
-                                "hover:[--d:100%]" : form[item.name as keyof typeof form] === "",
-                                "[--d:100%] hover:[--d:100%]" : form[item.name as keyof typeof form] !== "",
-                                "[background:linear-gradient(#477023_0_0)_bottom/var(--d,0)_3px_no-repeat]" : nickValid === false && item.name === "username",
-                                "[background:linear-gradient(#ff0000_0_0)_bottom/var(--d,0)_3px_no-repeat]" : nickValid === true && item.name === "username",
-                                "[background:linear-gradient(#bb0000_0_0)_bottom/var(--d,0)_3px_no-repeat]" : passValid === false && item.name === "password",
-                                "[background:linear-gradient(#477020_0_0)_bottom/var(--d,0)_3px_no-repeat]" : passValid === true && item.name === "password",
-                            })}>
-                            <label 
-                
-                            htmlFor=""
-                            //form[item.name as keyof FormStateType]
-                            className={form[item.name as keyof typeof form] === "" 
-                            ? "absolute duration-300 group-focus-within:-translate-y-6 z-0" 
-                            : "absolute duration-300 -translate-y-6 z-0 "}>
-                                {item.label}
-                            </label>
-                            { 
-                                item.name === "password" ?
+	return (
+		<>
+			<form className="flex flex-col rounded-xl w-full h-[65%] gap-7" onSubmit={handleSubmit}>
+				{
+					signInForm.map((item, index) => (
+						<div key={item.name} className={
+							clsx(
+								"relative flex gap-2 flex-col w-full mb-5 group border-b-0 [background:linear-gradient(#3b82f6_0_0)_bottom/var(--d,0)_3px_no-repeat] transition-all duration-500 [--d:0]", {
+								// For appearing a line when hovering
+								"hover:[--d:100%]": form[item.name as keyof typeof form] === "",
+								// Line sticks when something written
+								"[--d:100%] hover:[--d:100%]": form[item.name as keyof typeof form] !== "",
+								// TODO: Try to make this more optimizable cause' it look ugly
+								"[background:linear-gradient(#477023_0_0)_bottom/var(--d,0)_3px_no-repeat]": (nickValid === false && item.name === "username") || (emailValid === false && item.name === "email") || (passValid === false && item.name === "password"),
+								"[background:linear-gradient(#ff0000_0_0)_bottom/var(--d,0)_3px_no-repeat]": (nickValid === false && item.name === "username") || (emailValid === false && item.name === "email") || (passValid === false && item.name === "password"),
+							})}>
+							<label
+								//form[item.name as keyof FormStateType]
+								className={form[item.name as keyof typeof form] === ""
+									? "absolute duration-300 group-focus-within:-translate-y-6 z-0"
+									: "absolute duration-300 -translate-y-6 z-0 "
+								} >
+								{item.label}
+							</label>
+							<div className="flex flex-row">
+								<input
+									className="outline-none w-full h-full z-10"
+									type={
+										item.name === "password"
+											? passType
+												? "password" : "text"
+											: item.name
+									}
+									required
+									minLength={item.name === 'password' ? 8 : undefined}
+									id=""
+									autoComplete={item.name}
+									onChange={(e: React.ChangeEvent<any>) => {
+										switch (item.name) {
+											case "password":
+												setForm({ ...form, [item.name]: e.target.value })
+												e.target.value.length >= 8 && e.target.value.includes("&") && e.target.value != "" ? setPassValid(true) : setPassValid(false);
+											case "username": case "email":
+												const newValue = e.target.value;
+												setForm({ ...form, [item.name]: newValue });
 
-                                    <input 
-                                        className="outline-none w-full h-full z-10"
-                                        type="password" 
-                                        required minLength={8}
-                                        id="" 
-                                        autoComplete={item.name}
-                                        onChange={e => {
-                                            console.log(e.target.value);
-                                            setForm({...form, [item.name]: e.target.value})
-                                            e.target.value.length >= 8 && e.target.value.includes("&") ? setPassValid(true) : setPassValid(false);
-                                            console.log(passValid);
-                                        }}
-                                    />
-                                    
-                                    :
+												if (timeoutRefs.current[item.name]) {
+													console.log(timeoutRefs.current[item.name]);
+													clearTimeout(timeoutRefs.current[item.name]);
+												}
 
-                                    <input 
-                                    className="outline-none w-full h-full z-10"
-                                    required
-                                    type={item.type}
-                                    id="" 
-                                    autoComplete={item.name}
-                                    onChange={e => {
-                                        const newValue = e.target.value;
-                                        setForm({...form, [item.name]: newValue});
-                                        
-                                        if (timeoutRefs.current[item.name]) {
-                                            console.log(timeoutRefs.current[item.name]);
-                                            clearTimeout(timeoutRefs.current[item.name]);
-                                        }
-                                        
-                                        if (item.on_change && newValue.trim() !== "") {
-                                            timeoutRefs.current[item.name] = setTimeout(() => {
-                                                item.on_change!(newValue, setNickValid);
-                                            }, 1000); 
-                                        }
-                                    }}/>
-                                    
-                            }
-                            {item.name === "username" && nickValid === true && (
-                                <span className="text-red-500 text-xs absolute -bottom-4 left-0">
-                                    Username already exists
-                                </span>
-                            )}
-                            {item.name === "password" && passValid === false && form["password" as keyof FormStateType] !== "" && (
-                                <span className="text-red-500 text-xs absolute -bottom-4 left-0">
-                                    Password must be at least 8 characters and contain an "&" character
-                                </span>
-                            )}
-                        </div>  
-                    ))
-                }
-                <div className="flex w-full items-center justify-center">
-                    <button className={roboto.className + " shadow-md h-[60px] w-[120px] text-[20px] font-stretch-125% font-medium rounded-full cursor-pointer"} type="submit">
-                        Sign In
-                    </button>
-                </div>
-            </form>
-            <div className="cursor-pointer flex justify-center items-center">
-                <button
-                    className="cursor-pointer shadow-none h-[50px] w-[50px] rounded-xl flex items-center justify-center hover:shadow-sm duration-300 active:shadow-md"
-                    onClick={GitHubAuth}
-                    >
-                    <FaGithubAlt size="30px"/>
-                    <p></p>
-                </button>
-            </div>
-        </>
-    )
+												if (item.on_change && newValue.trim() !== "") {
+													timeoutRefs.current[item.name] = setTimeout(() => {
+														item.on_change!(newValue, item.name === "email" ? setNickValid : setEmailValid);
+													}, 1000);
+												}
+										}
+									}}
+								/>
+								{
+									item.name === "password"
+										?
+										<div className="h-[30px] cursor-pointer flex items-center justify-center z-9">
+											<button className="cursor-pointer h-full w-full"
+												type="button"
+												onClick={() => {
+													setPassType(!passType)
+												}}
+											>
+												{passType ? < FaEyeSlash size="17px" /> : < FaEye size="17px" />}
+											</button>
+										</div>
+										:
+										<></>
+								}
+							</div>
+							{item.name === "username" && nickValid === true && (
+								<span className="text-red-500 text-xs absolute -bottom-4 left-0">
+									Username already exists
+								</span>
+							)}
+							{item.name === "password" && passValid === false && form["password" as keyof FormStateType] !== "" && (
+								<span className="text-red-500 text-xs absolute -bottom-4 left-0">
+									Password must be at least 8 characters and contain special characters
+								</span>
+							)}
+						</div >
+					))
+				}
+				<div className="flex w-full items-center justify-center">
+					<button className={roboto.className + " shadow-md h-[60px] w-[120px] text-[20px] font-stretch-125% font-medium rounded-full cursor-pointer"} type="submit">
+						Sign In
+					</button>
+				</div>
+			</form >
+			<div className="cursor-pointer flex justify-center items-center gap-7">
+				<button
+					className="cursor-pointer shadow-sm h-[50px] w-[50px] rounded-xl flex items-center justify-center hover:bg-[#efe6de] duration-300 active:shadow-md"
+					onClick={GitHubAuth}
+				>
+					<FaGithub size="30px" />
+				</button>
+
+				<button
+					className="cursor-pointer shadow-sm h-[50px] w-[50px] rounded-xl flex items-center justify-center hover:bg-[#efe6de] duration-300 active:shadow-md"
+					onClick={GitHubAuth}
+				>
+					<FcGoogle size="30px" />
+				</button>
+			</div>
+		</>
+	)
 }

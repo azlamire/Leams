609237f1be040f1848b18ca1f37a99b0ad0bb028
azlamire <juchiLee@proton.me>
2025-10-20 12:04:39 +0300

    modified:   .gitignore 	deleted:    Makefile 	modified:   __pycache__/jwt_fin.cpython-312.pyc 	modified:   __pycache__/main.cpython-312.pyc 	modified:   __pycache__/oauth.cpython-312.pyc 	modified:   __pycache__/oauth_model.cpython-312.pyc 	modified:   __pycache__/register_test.cpython-312.pyc 	renamed:    jwt_fin.py -> app/core/jwt_fin.py 	renamed:    oauth.py -> app/core/oauth.py 	renamed:    database/__pycache__/core.cpython-312.pyc -> app/db/__pycache__/core.cpython-312.pyc 	renamed:    database/__pycache__/engine.cpython-312.pyc -> app/db/__pycache__/engine.cpython-312.pyc 	renamed:    database/__pycache__/models.cpython-312.pyc -> app/db/__pycache__/models.cpython-312.pyc 	renamed:    database/__pycache__/smod.cpython-312.pyc -> app/db/__pycache__/smod.cpython-312.pyc 	renamed:    database/__pycache__/tables.cpython-312.pyc -> app/db/__pycache__/tables.cpython-312.pyc 	renamed:    database/core.py -> app/db/core.py 	renamed:    database/models.py -> app/db/models.py 	new file:   app/dependencies.py 	renamed:    main.py -> app/main.py 	renamed:    models/__pycache__/jwt_schemes.cpython-312.pyc -> app/models/__pycache__/jwt_schemes.cpython-312.pyc 	renamed:    models/jwt_schemes.py -> app/models/jwt_schemes.py 	deleted:    conf.py 	deleted:    index.rst 	deleted:    jwt-private.pem 	deleted:    jwt-public.pem 	deleted:    make.bat 	modified:   oauth_model.py 	deleted:    parser/categories.py 	modified:   pyproject.toml 	deleted:    test.py 	modified:   uv.lock

--- app/db/core.py
+++ app/db/core.py
@@ -4,21 +4,18 @@ from fastapi import Depends, FastAPI, HTTPException, Query
 from sqlmodel import Field, Session, SQLModel, create_engine, select, table
 
 from dotenv import load_dotenv
-import os 
+import os
 
-load_dotenv("../.env")
+load_dotenv(".env")
 engine_url = os.getenv("SYNC_PSQL")
 
 engine = create_engine(engine_url)
+
 
 def get_session():
     with Session(engine) as session:
         yield session
 
+
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
-
-
-
-
-
--- test.py
+++
@@ -1,4 +0,0 @@
-from datetime import timedelta
-
-SECRET_KEY = open("jwt-private.pem").read()
-print(SECRET_KEY,timedelta(minutes=30))

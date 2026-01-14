20ebd18dee441be8d47dffaec5214365d37d583d
azlamire <juchiLee@proton.me>
2026-01-14 22:20:29 +0300

    For nixos development

--- flake.nix
+++ flake.nix
@@ -3,9 +3,9 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { nixpkgs, ... }:
+  outputs = { nixpkgs, ... }: 
     let
-      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib. genAttrs [ "x86_64-linux" "aarch64-linux" ];
     in {
       devShells = forAllSystems (system: 
         let
@@ -18,14 +18,20 @@
             ];
             
             env = {
-              LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
               UV_PYTHON_PREFERENCE = "only-system";
-              UV_PYTHON = "${pkgs.python312}/bin/python";
+              UV_PYTHON = "${pkgs. python312}/bin/python";
             };
             
+            NIX_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+              pkgs.stdenv.cc.cc
+              pkgs.zlib
+              pkgs.openssl
+            ];
+            NIX_LD = builtins.readFile "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
+            
             shellHook = ''
-              source .venv/bin/activate
               export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
+              source .venv/bin/activate
             '';
           };
         }
